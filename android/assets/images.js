f3cc7ffdcf059eb01d971c1e2da18eb5910cf085
Yashvi Shah <138695600+yashvishah27@users.noreply.github.com>
2024-01-31 16:57:20 +0530

    images.js

---
+++ android/assets/images.js
@@ -0,0 +1,82 @@
+export default {
+    logo: require('./images/logo.png'),
+    logintopbg: require('./images/top-bg.png'),
+    loginbottombg:require('./images/bottom-bg.png'),
+    // Dasboard
+    gym: require('./images/gym.jpg'),
+    diet: require('./images/diet.jpg'),
+    yoga: require('./images/yoga.jpg'),
+    bmi: require('./images/bmi.jpg'),
+    medi: require('./images/medi.jpg'),
+    trainer: require('./images/trainer.jpg'),
+    
+
+    // gym-Wormup
+    pressup: require('./images/pressup.gif'),
+    plank: require('./images/plank.gif'),
+    Deadbug: require('./images/deadbug.gif'),
+    Sideplank: require('./images/sideplankk.gif'),
+    Benchdips: require('./images/Benchdips.gif'),
+    Lowerbackcurl: require('./images/Lowerbackcurl.gif'),
+    Crunch: require('./images/crunches.gif'),
+    shadowboxgif: require('./images/shadowboxgif.gif'),
+    // gym-Basic
+    gym1: require('./images/gym1.png'),
+    gym2: require('./images/gym2.png'),
+    gym3: require('./images/gym3.png'),
+    gym4: require('./images/gym4.png'),
+    gym5: require('./images/gym5.png'),
+    gym6: require('./images/gym6.png'),
+    gym7: require('./images/gym7.png'),
+    gym8: require('./images/gym8.png'),
+    gym9: require('./images/gym9.png'),
+    // BICEP CURLS
+    ZottmanCurls: require('./images/ZottmanCurls.gif'),
+    ConcentrationCurls: require('./images/ConcentrationCurls.gif'),
+    HammerCurl: require('./images/HammerCurl.gif'),
+    BarDragCurl: require('./images/BarDragCurl.gif'),
+     // weight loss
+     barbellsquat: require('./images/barbellsquat.gif'),
+     bastards: require('./images/bastards.gif'),
+     battlerope: require('./images/battlerope.gif'),
+     benchpress: require('./images/benchpress.gif'),
+     bentoverrow: require('./images/bentoverrow.gif'),
+     cleannpress: require('./images/cleannpress.gif'),
+     lunge: require('./images/lunge.gif'),
+     romanian: require('./images/romanian.gif'),
+     situps: require('./images/situps.gif'),
+
+     // Simple Yoga
+    Parvatasan: require('./images/Parvatasan.png'),
+    bujangasan: require('./images/bujangasan.png'),
+    chakrasan: require('./images/chakrasan.png'),
+    dhanurasan: require('./images/dhanurasan.png'),
+    SuryaNamaskar: require('./images/SuryaNamaskar.jpg'),
+    //Weight loss Yoga
+    PrasaritaPadottanasana: require('./images/PrasaritaPadottanasana.jpg'),
+    sarvangasan: require('./images/sarvangasan.jpg'),
+    natarajasana: require('./images/natarajasana.jpg'),
+    Anjaneyasana: require('./images/Anjaneyasana.jpg'),
+    Vasisthasana: require('./images/Vasisthasana.jpg'),
+    treeasana: require('./images/treeasana.jpg'),
+    warriorthree: require('./images/warriorthree.jpg'),
+    lungevariationpose: require('./images/lungevariationpose.jpg'),
+    //beforeyoga
+    pyogatips: require('./images/pyogatips.png'),
+    marjariasana: require('./images/marjariasana.png'),
+    konasana: require('./images/konasana.png'),
+    trikonasana: require('./images/trikonasana.png'),
+    baddha: require('./images/baddha.png'),
+    //after
+    kandhasan: require('./images/kandhasan.jpg'),
+    Tadasana: require('./images/Tadasana.jpg'),
+     // Diet
+     YogaDaiet: require('./images/YogaDaiet.png'),
+     weightgain: require('./images/weightgain.png'),
+     wld: require('./images/wld.png'),
+     juice: require('./images/juice.png'),
+     teacup: require('./images/teacup.png'),
+     chilli: require('./images/chilli.jpg'),
+
+     Meditation: require('./images/Meditation.png'),
+};
